13ebf3ce99282511dbb721d0d1deaa19bfa637d6
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-08-25 23:35:42 +0300

    Edit the website's title, add some styles to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,39 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    // colors: {
-    //   body: "#F0FDF4",
-    // },
+    fontFamily: {
+      primary: ["Poppins", "sans-serif"],
+      gram: ["Gram", "sans-serif"],
+      Tsch: ["Tschichold", "sans-serif"],
+    },
+    container: {
+      padding: {
+        DEFAULT: "30px",
+        lg: "0",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        primary: {
+          500: "#7065f0",
+          600: "#5950c0",
+          700: "#4e46a8",
+          800: "#433c90",
+        },
+        headerBackground: "#fafafd",
+        background: "#f5f5fb",
+        border: "#e9e7f9",
+      },
+      transitionDuration: {
+        300: "300ms",
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
