5775e70beaf701e22902461311b13cc8a93c971d
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-09-13 18:08:18 +0300

    make the navbar responsive

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,22 +5,28 @@ import { notifications } from "./Notifications";
 import NavbarDropDown from "./NavbarDropDown";
 import { UserInfoContext } from "../../store/UserInfoProvider";
 import Spinner from "../common/Spinner";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Header = () => {
+  // users url location
   let location = useLocation();
+  // go to this url
   const navigateTo = useNavigate();
+  // show and hide the dropdown
   const [isDropDown, setIsDropDown] = useState(false);
+  // dropdown for language inside the dropdown
   const [isLanguage, setIsLanguage] = useState(false);
-  const [isActive, setIsActive] = useState(false);
+  // navbar scroll animation
+  const [isNavbarScroll, setIsNavbarScroll] = useState(false);
   const [userAuth, setUserAuth] = useState(false);
 
   // navbar animation
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY > 60) {
-        setIsActive(true);
+        setIsNavbarScroll(true);
       } else {
-        setIsActive(false);
+        setIsNavbarScroll(false);
       }
     });
     return () => {
@@ -28,10 +34,12 @@ const Header = () => {
     };
   }, []);
 
+  // get user from firebase auth
   const auth = getAuth();
-
+  // users data
   const { data, isLoading, isFetching, isError } = useContext(UserInfoContext);
 
+  // user information for the dropdown
   const [user, setUser] = useState({
     firstLetter: "",
     profilePhoto: "",
@@ -42,7 +50,9 @@ const Header = () => {
   const { firstLetter, profilePhoto, name, email } = user;
 
   useEffect(() => {
+    // if user logged in show the users information
     if (data && data !== undefined) {
+      // if user logged in with google set user's image to the dropdown
       if (data?.emailVerified) {
         setUser((prev) => ({
           ...prev,
@@ -51,6 +61,8 @@ const Header = () => {
           name: data?.displayName,
           email: data?.email,
         }));
+        setUserAuth(true);
+        // if user logged in with email and password, get users first name and set as an image
       } else {
         const fetchUser = async () => {
           const fullName = data.displayName;
@@ -66,16 +78,18 @@ const Header = () => {
         };
         fetchUser();
       }
-      setUserAuth(true);
+      // if user not Authenticated then set auth to false
     } else if (isError && isError.isNotAuthenticated) {
       setUserAuth(false);
     }
   }, [data]);
 
+  // loading spinner while user fetching is loading
   if (isLoading || isFetching) {
     return <Spinner />;
   }
 
+  // toggle dropdowns
   const toggleDropdown = () => {
     setIsDropDown(!isDropDown);
   };
@@ -83,13 +97,14 @@ const Header = () => {
     setIsLanguage(!isLanguage);
   };
 
+  // when user logout redirect user to the homepage
   const handleLogout = () => {
     try {
       auth.signOut(auth); // Sign out the current user and update the state
       navigateTo("/");
       window.location.reload();
     } catch (error) {
-      notifications("Sorry", "error");
+      notifications("You can't logout", true);
     }
   };
 
@@ -105,10 +120,10 @@ const Header = () => {
     <>
       <div
         className={`${
-          isActive ? "border-b py-1 shadow-md" : " py-0 "
+          isNavbarScroll ? "border-b py-1 shadow-md" : " py-0 "
         } sticky w-full bg-headerBackground border-b shadow top-0 z-40 transition-all duration-300 ease-in-out`}
       >
-        <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+        <header className="flex justify-between items-center px-3 max-w-6xl mx-auto max-sm:py-2">
           <div>
             <Link to="/">
               <h1 className="h-5 cursor-pointer text-2xl flex items-center justify-center font-bold">
@@ -117,7 +132,11 @@ const Header = () => {
             </Link>
           </div>
           <div>
-            <ul className="flex space-x-10 mr-[65px]">
+            <ul
+              className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+                isDropDown ? "top-12 pt-6" : "top-[-490px]"
+              }`}
+            >
               <Link to="/category/rent">
                 <li
                   className={`py-3 cursor-pointer transition duration-500 ease-in-out text-base font-semibold text-gray-500 border-b-[3px] border-b-transparent ${getActiveRouteStyles(
@@ -148,6 +167,7 @@ const Header = () => {
             </ul>
           </div>
           <div>
+            {/* <RxHamburgerMenu className="text-3xl font-extrabold text-gray-950" /> */}
             {userAuth ? (
               <NavbarDropDown
                 handleLogout={handleLogout}
