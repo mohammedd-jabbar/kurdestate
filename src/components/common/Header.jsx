3378b9fbf1203d310ea048a9455b0ee9de21b808
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-10-16 17:17:33 +0300

    remove console.logs

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -49,8 +49,6 @@ const Header = () => {
     setDarkSide(checked);
   };
 
-  // console.log(import.meta.env.VITE_GEOCODE_API);
-
   // navbar animation
   useEffect(() => {
     window.addEventListener("scroll", () => {
