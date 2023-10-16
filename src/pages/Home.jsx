d13abe84c4fafc0c49358d769442df5b83d93f57
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-10-16 17:20:03 +0300

    fix home slider animation duration

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -328,7 +328,7 @@ const Home = () => {
             <p className="text-gray-500 dark:text-slate-200 text-sm font-medium">
               {t("At Our Company, We Prioritize Your Satisfaction")}
             </p>
-            <Slide direction="left" delay={400} cascade>
+            <Slide direction="left" delay={250} cascade>
               <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full">
                 <div className="p-6 bg-white dark:bg-[#18212f] pb-12 transition-all duration-200 ease-in-out hover:shadow-xl hover:scale-105 rounded-lg shadow-md">
                   <p className="text-center">
